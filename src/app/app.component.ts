0c2f597bf72418fea7beef71a0833f86564edf6e
Bruno Beluco <beluco.bispo@hotmail.com>
2024-05-21 21:07:59 -0300

    feat: rodape component created + criar pensamento component created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CabecalhoComponent } from './cabecalho/cabecalho.component';
+import { RodapeComponent } from './rodape/rodape.component'
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, 
-    CabecalhoComponent
+    RouterOutlet,
+    RodapeComponent,
+    CriarPensamentoComponent,
+    CabecalhoComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
